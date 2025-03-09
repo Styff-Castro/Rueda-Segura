62f63fd9ca1e1c1d38091ea2a7e15c1de07f5339
Styff Castro <scastroh7@gmail.com>
2025-03-09 14:22:55 -0300

    Update Indices.sql
    Actualización de la tabla índice

--- Indices.sql
+++ Indices.sql
@@ -6,3 +6,8 @@ CREATE INDEX idx_tipo_vehiculo_id ON Vehiculos (tipo_vehiculo_id); -- ref de la 
 -- Índice en la tabla Seguro
 CREATE INDEX idx_numero_de_poliza ON Seguro (numero_de_poliza); -- ref de la columna numero de poliza en la tabla seguro
 
+-- Consultas de indices
+EXPLAIN SELECT * FROM Vehiculos WHERE matricula = '1234ABC';
+EXPLAIN SELECT matricula, marca, modelo FROM Vehiculos WHERE seguro_id = 2;
+EXPLAIN SELECT matricula, marca, modelo, kilometraje_actual FROM Vehiculos WHERE seguro_id = 3;
+EXPLAIN SELECT nombre, tipo_seguro, fecha_de_vencimiento FROM Seguro WHERE numero_de_poliza = 555555555;
